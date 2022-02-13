40edf9fa13f8c87560a1c855ba74fd6d40e72e61
gabei <gabe1792@gmail.com>
2022-02-13 11:58:29 -0600

    initial build setup for ToDo app. INstalled wepback and barious html and CSS loaders

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Yaba Daba ToDo',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
+      },
+    ],
+  },
+};
