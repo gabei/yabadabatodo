9aac5f0f118f9b4aeba35ccfd86ce09bae903279
gabei <gabe1792@gmail.com>
2022-02-16 15:18:23 -0600

    began basic sidebar creation

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,4 +3,11 @@ import './Sidebar.scss';
 
 const Sidebar = createElement('div', 'Sidebar');
 
+const title = createElement('h3', 'Sidebar__title');
+title.textContent = 'Projects';
+Sidebar.append(title);
+
+const list = createElement('ul', 'Sidebar__list');
+Sidebar.append(list);
+
 export default Sidebar;
