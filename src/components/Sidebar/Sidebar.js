4ea8b57ed6f7cc3d56272dcea9dc3a7b577773f8
gabei <gabe1792@gmail.com>
2022-02-16 15:33:06 -0600

    cahnged style of sidebar elements. Removed flex to force items to stack (as they should). Flex not necessary

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,11 +3,19 @@ import './Sidebar.scss';
 
 const Sidebar = createElement('div', 'Sidebar');
 
-const title = createElement('h3', 'Sidebar__title');
+const title = createElement('h4', 'Sidebar__title');
 title.textContent = 'Projects';
 Sidebar.append(title);
 
 const list = createElement('ul', 'Sidebar__list');
 Sidebar.append(list);
+
+const projectList = ['Project1', 'Project2', 'Project3', 'Project4'];
+
+projectList.forEach((item) => {
+  let project = createElement('li');
+  project.textContent = item;
+  list.append(project);
+});
 
 export default Sidebar;
