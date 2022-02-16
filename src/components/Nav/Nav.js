94477a4c6f05bdd11cffcb30d0d9a75781f53d48
gabei <gabe1792@gmail.com>
2022-02-16 15:02:58 -0600

    added a nav logo in the corner

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,6 +7,7 @@ const navLogo = createElement('div', 'Nav__logo');
 Nav.append(navLogo);
 
 const logoImage = createElement('img');
+logoImage.src = '../src/assets/logo-white.png';
 navLogo.append(logoImage);
 
 export default Nav;
