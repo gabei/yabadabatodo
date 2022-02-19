22a8a661dcabad48d3fdf94c67352b7cae3c6efd
gabei <gabe1792@gmail.com>
2022-02-19 12:58:29 -0600

    added a controller for interacrting with storage and ui elements

---
+++ src/controller/controller.js
@@ -0,0 +1,39 @@
+function createProject() {
+  //create a new project name and add it to storage
+}
+
+function addProjectToView() {
+  // create project information in the sidebar with event listeners
+}
+
+function removeProject() {
+  // remove project from storage
+}
+
+function removeProjectFromView() {
+  // remove project from sidebar
+}
+
+function createTask() {
+  // create a new task and add to storage
+}
+
+function addTaskToView() {
+  // add the task to work area
+}
+
+function removeTask() {
+  // remove task from storage
+}
+
+function removeTaskFromView() {
+  // remove task from work area
+}
+
+function showProject() {
+  // show the selected project in the work area
+}
+
+function showTask() {
+  // show a detailed view of the selected task
+}
