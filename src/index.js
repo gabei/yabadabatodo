d4f62660c175717d3c9b63b6cc66782f47124fe9
gabei <gabe1792@gmail.com>
2022-02-19 12:45:52 -0600

    removed the Container folder and file and confined it to the index.js.No need for it.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import Container from './Container/Container.js';
-import './style/main.scss';
 import { initStorage } from './storage/storage';
+import { createElement } from './ui/ui';
+
+import Nav from './components/Nav/Nav';
+import Sidebar from './components/Sidebar/Sidebar';
+import WorkArea from './components/WorkArea/WorkArea';
+
+import './style/main.scss';
+
+const Container = createElement('div', 'Container');
+Container.append(Nav, Sidebar, WorkArea);
+document.body.append(Container);
 
 initStorage();
-
-document.body.append(Container);
